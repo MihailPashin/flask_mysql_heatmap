7602ce023585a3c7ddd4ced5a09ba139a01ce3d5
MihailPashin <michalpoisonish@gmail.com>
2024-04-29 22:08:43 +0300

    АвтоСкрипт dataloader.py для записи из Json в БД

---
+++ app/dataloader.py
@@ -0,0 +1,63 @@
+import json
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import MetaData
+import math
+
+
+
+
+def load_database(db, app):
+    with app.app_context():
+        exSession = sessionmaker(bind=db.engine)
+        session = exSession()
+        groups_zhilya = []
+        metadata = MetaData()
+        metadata.reflect(bind=db.engine)
+        print(metadata.tables)
+        
+        try:
+            with open('app/storage/groups.json', 'r') as f:
+                groups = json.load(f)
+                print('thats fine')
+
+            
+            for k, v in enumerate(groups):
+
+                print(f'groups_zhilya {k},{v}')
+                stmt = metadata.tables['groups_home'].insert().values(id=v['group_id'],name=v['Group'])
+                db.session.execute(stmt)
+                db.session.commit()
+
+            with open('app/storage/objects_nedvizhimost.json', 'r') as f:
+                objects_nedvizhimost = json.load(f);
+                print('thats fine');
+
+
+            for k, v in enumerate(objects_nedvizhimost):
+
+                print(f'objects_nedvizhimost {k},{v}')
+                stmt = metadata.tables['objects_estate'].insert().values( id = v['object_id'], \
+                name = v['title'],coord_x = v['coord_X'] ,coord_y = v['coord_Y'])
+                db.session.execute(stmt)
+                db.session.commit()
+   
+            with open('app/storage/final_result.json', 'r') as f:
+                summary = json.load(f);
+                print('thats fine');
+
+            
+            for k, v in enumerate(summary):
+
+                print(f'summary {k},{v}')
+                stmt = metadata.tables['summary'].insert().values( raions_id = v['object_id'], \
+                group_id = v['group_id'],likeness = math.ceil(v['x_scaled']))
+                db.session.execute(stmt)
+                db.session.commit()
+
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            session.rollback()
+        finally:
+            session.close()
--- app/tables.py
+++ app/tables.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import relationship,backref
 class Raions(db.Model):
     __tablename__ = 'objects_estate'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80),  nullable=False)
     coord_x = db.Column(db.Float,primary_key=True)
     coord_y = db.Column(db.Float,primary_key=True)
 
     def __repr__(self):
-        return f"<Raions(name='{self.name}', department='{coord_x}', '{coord_y}' )>"
+        return f"<Raions(name='{self.name}', department='{self.coord_x}', '{self.coord_y}' )>"
     
 class Group_Home(db.Model):
     __tablename__ = 'groups_home'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
@@ -22,9 +22,9 @@ class Group_Home(db.Model):
 
 class Summary(db.Model):
     __tablename__ = 'summary'
-    id = db.Column(db.Integer, primary_key=True)
-    raions_id = db.Column(db.Integer, db.ForeignKey('objects_estate.id'),primary_key=True)
-    group_id = db.Column(db.Integer, db.ForeignKey('groups_home.id'),primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    raions_id = db.Column(db.Integer, db.ForeignKey('objects_estate.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('groups_home.id'))
     likeness = db.Column(db.Integer)
     raions = relationship("Raions", backref=backref('summary', cascade="all, delete-orphan"))
     group = relationship("Group_Home", backref=backref('summary', cascade="all, delete-orphan"))
